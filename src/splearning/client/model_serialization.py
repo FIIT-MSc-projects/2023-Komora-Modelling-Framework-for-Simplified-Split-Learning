a3c3c6507a9de9032c86fbadf3622123e69aa75d
misosviso <michal.komora@protonmail.com>
2023-11-11 19:42:37 +0100

    Loading client models from YAML files 2.0 -- refactor 2.0

--- src/splearning/client/model_serialization.py
+++ src/splearning/client/model_serialization.py
@@ -1,42 +1,60 @@
 import os
-from torchinfo import summary
 import yaml
+import torch.nn as nn
 import torch
-from torch.nn import *
-import torch.nn.functional as F
 import collections
 
-class ModelBuilder():
-    def __init__(self):
-        self.mapping = collections.ChainMap({
-            "Conv2d": Conv2d,
-            "MaxPool2d": MaxPool2d,
-            "Linear": Linear,
-            "ReLU": ReLU
-        })
+LAYER_MAPPING = {
+    "Conv1d": nn.Conv1d,
+    "Conv2d": nn.Conv2d,
+    "Conv3d": nn.Conv3d,
+    "Linear": nn.Linear,
+    "BatchNorm1d": nn.BatchNorm1d,
+    "BatchNorm2d": nn.BatchNorm2d,
+    "BatchNorm3d": nn.BatchNorm3d,
+    "MaxPool1d": nn.MaxPool1d,
+    "MaxPool2d": nn.MaxPool2d,
+    "MaxPool3d": nn.MaxPool3d,
+    "AvgPool1d": nn.AvgPool1d,
+    "AvgPool2d": nn.AvgPool2d,
+    "AvgPool3d": nn.AvgPool3d,
+    "ReLU": nn.ReLU,
+    "LeakyReLU": nn.LeakyReLU,
+    "PReLU": nn.PReLU,
+    "Sigmoid": nn.Sigmoid,
+    "Tanh": nn.Tanh,
+    "Softmax": nn.Softmax,
+    "LogSoftmax": nn.LogSoftmax,
+    "Dropout": nn.Dropout,
+    "Dropout2d": nn.Dropout2d,
+    "Dropout3d": nn.Dropout3d,
+    "Embedding": nn.Embedding,
+    "LSTM": nn.LSTM,
+    "GRU": nn.GRU,
+    "RNN": nn.RNN,
+    "Transformer": nn.Transformer,
+    "TransformerEncoder": nn.TransformerEncoder,
+    "TransformerDecoder": nn.TransformerDecoder,
+}
 
-    def build_model(self, layer):
-        model, params = list(layer.items())[0]
-        if params is None:
-            params = {}
-        return self.mapping[model](**params)
+def create_layer(layer):
+    model, params = list(layer.items())[0]
+    if params is None:
+        params = {}
+    return LAYER_MAPPING[model](**params)
 
-def serialize_model(model: torch.nn.Module.__class__, model_path: str):
+def serialize_model(model: nn.Module.__class__, model_path: str):
     if not os.path.isdir(os.path.dirname(model_path)):
         os.makedirs(os.path.dirname(model_path), exist_ok=True)    
     torch.save(model(), model_path)
 
 def create_model(layers):
-    builder = ModelBuilder()
-    
-    model_layers = [builder.build_model(layer) for layer in layers]
-    model = Sequential(*model_layers)
-    
+    model_layers = [create_layer(layer) for layer in layers]
+    model = nn.Sequential(*model_layers)
     return model
 
 
 def load_model_from_yaml(path):
-    
     with open(path, 'r') as file:
         client_config = yaml.safe_load(file)
         model = create_model(client_config["layers"])
